e4c7922a522a4797cf5ad8f3442726f73bb7ea0c
Marius Miron <marius.costel.miron@gmail.com>
2014-11-16 20:22:45 +0200

    1. creating Director model
    2. creating migration file

---
+++ app/models/Director.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marius
+ * Date: 16.11.2014
+ * Time: 19:21
+ */
+
+class Director extends Eloquent{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'directors';
+
+}
